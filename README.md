75d327b95293ff2726857ed03e4d8aacaa50bd5d
Fabrizio Marino <fb.marino@hotmail.it>
2022-11-12 14:33:54 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # How to run
 > npm run start
+or...
+> ts-node build/index.ts
+
+Check output folder, example result:
+![example](outout/graph.png "Example graph generated")
--- dist/graph-renderer-layout.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GraphRendererLayout = void 0;
-var GraphRendererLayout;
-(function (GraphRendererLayout) {
-    GraphRendererLayout[GraphRendererLayout["Graph"] = 0] = "Graph";
-    GraphRendererLayout[GraphRendererLayout["Tree"] = 1] = "Tree";
-})(GraphRendererLayout = exports.GraphRendererLayout || (exports.GraphRendererLayout = {}));
