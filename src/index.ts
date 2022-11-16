364a82b37d77ad755c2a5328565373fc79f07083
Fabrizio Marino <fb.marino@hotmail.it>
2022-11-16 15:38:31 +0100

    added cli arguments

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,24 @@ import {VSCODE_THEME} from "./renderer-themes";
 import {GraphRendererLayout} from "./renderer-layout";
 import {GraphParser} from "./parser";
 import * as path from "path";
+import yargs from "yargs";
+
+const argv = yargs
+    .option('template', {
+        description: 'the json template config file',
+        alias: 't',
+        type: 'string'
+    })
+    .option('answer_set', {
+        description: 'the json answer set file',
+        alias: 'a',
+        type: 'string'
+    })
+    .option('output_dir', {
+        description: 'the output dir',
+        alias: 'o',
+        type: 'string'
+    }).parseSync();
 
 const renderer = new GraphRenderer();
 renderer.width = 1280;
@@ -12,29 +30,26 @@ renderer.height = 1280;
 renderer.theme = VSCODE_THEME;
 renderer.layout = GraphRendererLayout.Tree;
 
-const templatePath = path.join(__dirname, "../input/template.json");
-const wrapperPath = path.join(__dirname, "../input/wrapper.json");
-const graphParser = new GraphParser(templatePath, wrapperPath);
+const templatePath = argv.template || path.join(__dirname, "../input/example-template.json");
+const answerSetPath = argv.answer_set || path.join(__dirname, "../input/example-wrapper.json");
+const outputDirPath = argv.output_dir || path.join(__dirname, "../output");
+const graphParser = new GraphParser(templatePath, answerSetPath);
 
-console.log("Using " + path.basename(templatePath) + " as template...");
-console.log("Using " + path.basename(wrapperPath) + " as answer set data...");
+console.log("Using " + templatePath + " as template file...");
+console.log("Using " + answerSetPath + " as answer set file...");
+console.log("Using " + outputDirPath + " as output directory...");
 
 try {
-    const renderingPromises: Promise<OnFinish>[] = [];
-
     graphParser.toGraphs().forEach((graph: Graph, index) => {
         console.log(`Rendering graph ${index}...`);
 
-        let rendering = renderer.render(graph);
-        rendering.then((img: OnFinish) => {
-            const filepath = 'output/graph-' + index + '-' + Date.now() + '.png';
+        renderer.render(graph).then((img: OnFinish) => {
+            const filename = 'graph-' + index + '-' + Date.now() + '.png';
+            const filepath = outputDirPath + '/' + filename;
             fs.writeFileSync(filepath, img.base64Data, 'base64');
-            console.log(`Graph ${index} saved as ${filepath}`);
+            console.log(`Graph ${index} saved as ${filename}`);
         });
-        renderingPromises.push(rendering);
     });
-
-    Promise.all(renderingPromises).then(() => process.exit(1));
 } catch (error) {
     if (error instanceof Error) {
         console.error(error.message);
