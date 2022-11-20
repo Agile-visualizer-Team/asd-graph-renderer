7c264875944f8e4ef1f78df44080e77cb0ed4dc2
AlexSal-sudo <slrlsn00r12d086f@studenti.unical.it>
2022-11-20 12:48:44 +0100

    Merge by Alex, Comments added

--- src/parser.ts
+++ src/parser.ts
@@ -1,24 +1,42 @@
 import fs from 'fs'
 import {Graph, GraphEdge, GraphNode} from "./models";
+import yargs from 'yargs/yargs'
+import {hideBin} from 'yargs/helpers'
+import readlineSync from 'readline-sync'
 
 export class GraphParser {
     private template_path: string;
-    private dlv_path: string;
+    private dlv_path: string = "NONE";
     private SYNTAX_ERROR: string = "Template Error: The template file is not syntactically right. Please check it!"
+    private argv = yargs(hideBin(process.argv)).options({input: {type: 'boolean', default: false}, json: {type: 'boolean', default: false}},).parseSync();
 
+/**
+ * The constructor takes two arguments, a template_path and a dlv_path. If the template_path doesn't
+ * exist, it throws an error. If the dlv_path doesn't exist, it throws an error. If the template_path
+ * exists, it sets the template_path to the template_path. If the dlv_path exists, it sets the dlv_path
+ * to the dlv_path.
+ * @param {string} template_path - The path to the template file.
+ * @param {string} dlv_path - The path to the dlv executable.
+ */
     constructor(template_path: string, dlv_path: string) {
         if (!fs.existsSync(template_path))
             throw Error("Please, check the template_path you have given!");
         if (!fs.existsSync(dlv_path))
             throw Error("Please, check the dlv_path you have given!");
         this.template_path = template_path;
-        this.dlv_path = dlv_path;
+        if(!this.get_input_flag())
+            this.dlv_path = dlv_path;
     }
 
+/**
+ * It takes a JSON file, and returns a JSON file.
+ * @param {string|null} [outputFile=null] - The file to write the output to. If null, the output will
+ * be returned as a string.
+ * @returns The return value is a Promise.
+ */
     getAnswerSet(outputFile: string|null = null) {
         try {
             const options = this.getJSON(this.template_path);
-            //console.log("JSON builded successfully. Check in your folder...")
             return this.buildOutput(options, outputFile);
         } catch (err) {
             if (err instanceof Error) {
@@ -27,11 +45,41 @@ export class GraphParser {
         }
     }
 
+/**
+ * Private get_json_flag(){
+ *         return this.argv.json;
+ *     }
+ * @returns The value of the json property of the argv object.
+ */
+    private get_json_flag(){
+        return this.argv.json;
+    }
+
+/**
+ * Private get_input_flag(){
+ *         return this.argv.input;
+ *     }
+ * @returns The input flag.
+ */
+    private get_input_flag(){
+        return this.argv.input;
+    }
+
+/**
+ * It takes a JSON object with two properties, nodes and arch, and returns an array of JSON objects
+ * with two properties, nodes and arch. The nodes and arch properties are arrays of strings. The
+ * strings are atoms. The atoms are extracted from the answer sets of a dlv program. The dlv program is
+ * generated from the JSON object
+ * @param {any} options - {
+ * @param {string|null} [outputFile=null] - the file to write the output to. If null, the output is
+ * returned as a string.
+ * @returns An array of objects. Each object has two properties: nodes and arch.
+ */
     private buildOutput(options: any, outputFile: string|null = null) {
         const nodes = this.checkAtomsSyntax(options.nodes);
         const arch = this.checkAtomsSyntax(options.arch);
-        const node_atom = nodes[0], node_ariety = nodes[1];
-        const arch_atom = arch[0], arch_ariety = arch[1];
+        const node_atom = new RegExp(nodes[0]+'\(.+\)'), node_ariety = nodes[1];
+        const arch_atom = new RegExp(arch[0]+'\(.+\)'), arch_ariety = arch[1];
         const answerSet = this.getJSON(this.dlv_path);
         let output = [];
         for (var i = 0; i < answerSet.length; ++i) {
@@ -39,9 +87,9 @@ export class GraphParser {
             const a: string[] = [];
             let _as = answerSet[i].as;
             for (var j = 0; j < _as.length; ++j) {
-                if (_as[j].split("(")[0] === node_atom && _as[j].split(",").length == node_ariety) {
-                    n.push(_as[j]);
-                } else if (_as[j].split("(")[0] === arch_atom && _as[j].split(",").length == arch_ariety)
+                if (node_atom.test(_as[j]) && _as[j].split(",").length == node_ariety) 
+                    n.push(_as[j]);               
+                else if (arch_atom.test(_as[j]) && _as[j].split(",").length == arch_ariety)
                     a.push(_as[j]);
             }
             if (n.length != 0) {
@@ -54,10 +102,32 @@ export class GraphParser {
         return output
     }
 
-    private getJSON(path: string) {
+/**
+ * If the user has specified that they want to input the JSON file in the command line, then we ask them for the JSON file,
+ * otherwise we just require it.
+ * @param {string} path - The path to the JSON file.
+ * @returns The return value is a JSON object.
+ */
+    getJSON(path: string){
+        if(this.get_input_flag() && path === this.dlv_path){
+            var res = readlineSync.question('');
+            return JSON.parse(res);
+        }
+
         return require(path);
     }
 
+/**
+ * "If the node_string doesn't match the regex, throw an error. Otherwise, return the node_string split
+ * by the slash."
+ * 
+ * The regex is a simple check to make sure the node_string is in the format of "node/1".
+ * 
+ * The function is private because it's only used internally by the class.
+ * 
+ * @param {string} node_string - The string that is being checked for syntax.
+ * @returns The return value is an array of strings.
+ */
     private checkAtomsSyntax(node_string: string) {
         if (!/^\S+\/\d+$/.test(node_string)) {
             throw new Error(this.SYNTAX_ERROR);
@@ -66,6 +136,11 @@ export class GraphParser {
         return node_string.split("/");
     }
 
+/**
+ * It takes a template file, runs it through the ASP solver, and then parses the output into a list of
+ * graphs
+ * @returns An array of Graphs.
+ */
     toGraphs(): Graph[] {
         const options = this.getJSON(this.template_path);
         const answerSets = this.buildOutput(options);
--- test/parser.ts
+++ test/parser.ts
@@ -1,106 +1,101 @@
-import {GraphParser} from "../src/parser";
-import {expect} from "chai";
+import { GraphParser } from "../src/parser";
+import { expect } from "chai";
 import * as sinon from "sinon";
 import fs from "fs";
-import path from "path";
+import readSync from "readline-sync";
 
+/* A constant that is used to test the parser. */
 const GOOD_AS = [
-    {
-        as: ["node(1)", "cell(74,5)"],
-        cost: "1@2 2@3",
-    },
-    {
-        as: ["node(7)", "arch(7,7)"],
-        cost: "1@2",
-    },
+  {
+    as: ["node(1)", "cell(74,5)"],
+    cost: "1@2 2@3",
+  },
+  {
+    as: ["node(7)", "arch(7,7)"],
+    cost: "1@2",
+  },
 ];
 
+/* A constant that is used to test the parser. */
 const GOOD_TEMPLATE = {
-    nodes: "node/1",
-    arch: "arch/2",
+  nodes: "node/1",
+  arch: "arch/2",
 };
 
-function graph_fixture(
-    template: any = GOOD_TEMPLATE,
-    wrapper: any = GOOD_AS,
-    outputFile: string|null = null
-) {
-    const fs_stub = sinon.stub(fs, "existsSync").returns(true);
-    let g = new GraphParser("", "");
-    const stub = sinon
-        .stub(g, <any>"getJSON")
-        .onCall(0)
-        .returns(template)
-        .onCall(1)
-        .returns(wrapper);
-    const out = g.getAnswerSet(outputFile);
-    stub.restore();
-    fs_stub.restore();
-    return out;
+/**
+ * It takes a template and a wrapper, and returns the output of the graph parser.
+ * @param {any} template - the template file
+ * @param {any} wrapper - the JSON object that is returned by the getJSON method
+ * @param {boolean} [flag=false] - boolean = false
+ * @returns The return value of the function.
+ */
+function graph_Fixture(template: any = GOOD_TEMPLATE,wrapper: any = GOOD_AS,flag: boolean = false) {
+  sinon.stub(fs, "existsSync").returns(true);
+  let g = new GraphParser("test1", "test2");
+  sinon.stub(g, <any>"get_json_flag").returns(flag);
+  sinon.stub(g, <any>"getJSON").onCall(0).returns(template).onCall(1).returns(wrapper);
+  const out = g.getAnswerSet();
+  return JSON.stringify(out);
 }
-
+/**
+ * It takes a list of nodes and a list of archs, and returns a list of objects, each of which has a
+ * list of nodes and a list of archs
+ * @returns The expected result.
+ * </code>
+ */
 function get_expected() {
-    return JSON.stringify([
-        {nodes: ["node(1)"], arch: []},
-        {nodes: ["node(7)"], arch: ["arch(7,7)"]},
-    ]);
+  return JSON.stringify([
+    { nodes: ["node(1)"], arch: [] },
+    { nodes: ["node(7)"], arch: ["arch(7,7)"] },
+  ]);
 }
 
 describe("Parser_Test", () => {
-    it("should throw an error if you try to access an invalid path", () => {
-        expect(function () {
-            new GraphParser("", "");
-        }).to.throw(Error, "Please, check the template_path you have given!");
+  afterEach(function(){
+    sinon.restore();
+  })
+  it("should throw an error if you try to access an invalid template path", () => {
+    expect(function () {
+      new GraphParser("", "");
+    }).to.throw(Error, "Please, check the template_path you have given!");
+  }),
+  it("should throw an expection if you try to access an invalid dlv_path", ()=>{
+    sinon.stub(fs, "existsSync").onCall(0).returns(true).onCall(1).returns(false);
+    expect(function(){
+      let g = new GraphParser("test1", "test2");
+    }).to.throw(Error, "Please, check the dlv_path you have given!")
+  })
+  it("should throw an exception if the template file is not well formatted", () => {
+    sinon.stub(fs, "existsSync").returns(true);
+    let g = new GraphParser("test1","test2");
+    sinon.stub(g, <any>"getJSON").returns({ nodes: "asf_3", arch: "try&2" });
+    expect(function () {
+      g.getAnswerSet();
+    }).to.throw(
+      "Template Error: The template file is not syntactically right. Please check it!"
+    );
+  }),
+  it("should fetch the json file properly and get the answersets", () => {
+    const expected = get_expected();
+    const out = graph_Fixture();
+    expect(out).equal(expected);
+  }),
+  it("should save the answerset in a JSON file and be coherent", () => {
+    const out = graph_Fixture(GOOD_TEMPLATE, GOOD_AS, true);
+    expect(fs.existsSync("./graph.json")).to.be.true;
+    const expected = get_expected();
+    expect(out).equal(expected);
+  }),
+  it('should get the input from stdin and provide the correct answer set', () => {
+    sinon.stub(fs, "existsSync").returns(true);
+    let g = new GraphParser("test1", "test2");
+    sinon.stub(readSync,"question").returns(JSON.stringify(GOOD_AS));
+    sinon.stub(g, <any>"get_input_flag").returns(true);
+    const t = sinon.stub(g, <any>"getJSON").onCall(0).returns(GOOD_TEMPLATE).onCall(1).callsFake(()=>{
+      t.restore()
+      return g.getJSON("test2");
     });
-
-    it("should throw an exception if the template file is not well formatted", () => {
-        const fs_stub = sinon.stub(fs, "existsSync").returns(true);
-        let g = new GraphParser(
-            "stub_template_path",
-            "stub_dlv_path"
-        );
-        const stub = sinon
-            .stub(g, <any>"getJSON")
-            .returns({nodes: "asf_3", arch: "try&2"});
-        expect(function () {
-            g.getAnswerSet();
-        }).to.throw(
-            "Template Error: The template file is not syntactically right. Please check it!"
-        );
-        stub.restore();
-        fs_stub.restore();
-    });
-
-    it("should fetch the json file properly and get the answersets", () => {
-        const expected = get_expected();
-        const out = JSON.stringify(graph_fixture());
-        expect(out).equal(expected);
-    });
-
-    it("should save the answerset in a JSON file and be coherent", () => {
-        const outputPath = path.join(__dirname, "parser_graph.json");
-        const out = graph_fixture(GOOD_TEMPLATE, GOOD_AS, outputPath);
-        expect(fs.existsSync(outputPath)).to.be.true;
-        const expected = get_expected();
-        expect(JSON.stringify(out)).equal(expected);
-    });
-
-    it("should parse the answer set correctly", () => {
-        const AS = [
-            {
-                as: ["blocco(1)", "blocco(2)", "arco(1,2)"],
-                cost: "1@2",
-            },
-        ];
-        //Expected output
-        const template = {
-            nodes: "blocco/1",
-            arch: "arco/2",
-        };
-        console.log("AS: ", AS);
-        console.log("Template: ", template);
-
-        let out = graph_fixture(template, AS);
-        console.log(out);
-    });
+    const out = g.getAnswerSet();
+    expect(JSON.stringify(out)).to.eq(get_expected())
+  })
 });
