f61e268da4497173dbb5f38bc8e30e1b30934f48
Fabrizio Marino <fb.marino@hotmail.it>
2022-12-17 15:06:56 +0100

    typo fix

--- test/expressions.ts
+++ test/expressions.ts
@@ -6,15 +6,15 @@ import * as sinon from "sinon";
 
 describe("EXPRESSIONS TEST", () => {
     it("should return bar", () => {
-        const expression: Expression = <Expression>{if: [], else: 'bar'};
-        const variables: GraphVariables = <GraphVariables>{};
+        const expression: Expression = {if: [], else: 'bar'};
+        const variables: GraphVariables = {};
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('bar');
     });
     it("should return bar (condition type is missing, so the if condition is invalid)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 'hello'
         };
         let spy = sinon.spy(console, 'error');
@@ -27,45 +27,45 @@ describe("EXPRESSIONS TEST", () => {
      */
 
     it("should return foo (hello matches exactly hello)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', matches: 'hello', then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 'hello'
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('foo');
     });
     it("should return bar (HELLO does not match exactly hello because matches is case sensitive)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', matches: 'HELLO', then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 'hello'
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('bar');
     });
     it("should return pippo (only the second if condition is true)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [
                 <ExpressionCondition>{variable: 'a', matches: 'hello', then: 'foo'},
                 <ExpressionCondition>{variable: 'a', matches: 'HELLO', then: 'pippo'}
             ],
             else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 'HELLO'
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('pippo');
     });
     it("should return foo (the first if condition is true, the second one must be ignored)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [
                 <ExpressionCondition>{variable: 'a', imatches: 'hello', then: 'foo'},
                 <ExpressionCondition>{variable: 'a', matches: 'HELLO', then: 'pippo'}
             ],
             else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 'HELLO'
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('foo');
@@ -76,19 +76,19 @@ describe("EXPRESSIONS TEST", () => {
      */
 
     it("should return bar (matches is undefined so the evaluation must be false)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', imatches: undefined, then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 'hello'
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('bar');
     });
     it("should return foo (hello matches HELLO because imatches is used)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', imatches: 'HELLO', then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 'hello'
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('foo');
@@ -99,10 +99,10 @@ describe("EXPRESSIONS TEST", () => {
      */
 
     it("should return foo (the variable mycoolgraph contains cool)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', contains: 'cool', then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 'mycoolgraph'
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('foo');
@@ -113,20 +113,20 @@ describe("EXPRESSIONS TEST", () => {
      */
 
     it("should return foo (the variable MYCOOLGRAPH contains cool because icontains is used)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', icontains: 'cool', then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 'MYCOOLGRAPH'
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('foo');
     });
 
     it("should return bar (icontains is undefined)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', icontains: undefined, then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 'MYCOOLGRAPH'
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('bar');
@@ -137,28 +137,28 @@ describe("EXPRESSIONS TEST", () => {
      */
 
     it("should return foo (the variable a is less than 51)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', lt: 51, then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 50
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('foo');
     });
     it("should return bar (the variable a is not less than 51)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', lt: 51, then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 52
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('bar');
     });
     it("should return bar (lt is undefined)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', lt: undefined, then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 50
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('bar');
@@ -169,28 +169,28 @@ describe("EXPRESSIONS TEST", () => {
      */
 
     it("should return foo (the variable a is less or equal than 51)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', lte: 51, then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 51
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('foo');
     });
     it("should return bar (the variable a is not less or equal than 51)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', lte: 51, then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 52
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('bar');
     });
     it("should return bar (lte is undefined)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', lte: undefined, then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 50
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('bar');
@@ -201,29 +201,29 @@ describe("EXPRESSIONS TEST", () => {
      */
 
     it("should return foo (the variable a is greater than 51)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', gt: 50, then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 51
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('foo');
     });
     it("should return bar (the variable a is not greater than 51)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', gt: 52, then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 51
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('bar');
     });
 
     it("should return bar (gt is undefined)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', gt: undefined, then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 50
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('bar');
@@ -234,29 +234,29 @@ describe("EXPRESSIONS TEST", () => {
      */
 
     it("should return foo (the variable a is greater or equal than 51)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', gte: 51, then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 51
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('foo');
     });
     it("should return bar (the variable a is not greater or equal than 51)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', gte: 52, then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 51
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('bar');
     });
 
     it("should return bar (gte is undefined)", () => {
-        const expression: Expression = <Expression>{
+        const expression: Expression = {
             if: [<ExpressionCondition>{variable: 'a', gte: undefined, then: 'foo'}], else: 'bar'
         };
-        const variables: GraphVariables = <GraphVariables>{
+        const variables: GraphVariables = {
             a: 50
         };
         expect(new ExpressionEvaluator(expression).evaluate(variables)).to.be.equal('bar');
